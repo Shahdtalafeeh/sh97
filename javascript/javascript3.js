587e6fb18510e7ab03831f1cbb90ff5f51cebd44
shahd_altalafeeh <shahdmoh16@gmail.com>
2022-10-28 15:27:36 +0200

    d

---
+++ javascript/javascript3.js
@@ -0,0 +1,22 @@
+let name="Lorem ipsum dolor sit amet"
+console.log(name.endsWith('t'));
+console.log(name.endsWith('o',16));
+console.log(name.startsWith('L'));
+console.log(name.startsWith('e',3));
+console.log(name.length);
+console.log(name.includes('amet'));
+console.log(name[3]);
+console.log(name.charAt(4));
+console.log(name.indexOf('u'));
+console.log(name.indexOf('u',2));
+console.log(name.lastIndexOf('m'));
+console.log(name.lastIndexOf('m',15));
+console.log(name.slice(5,15));
+console.log(name.split(' '));
+console.log(name.split('i'));
+console.log(name.substr(7,4));
+
+
+
+
+
